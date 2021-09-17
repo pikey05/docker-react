3ba1c7af3071bd1c8d3022cafd2d4f922cb7ec28
Sean Cook <smcook55@gmail.com>
2021-09-17 11:30:01 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Test!
+        </p>
+      </header>
+      <p className="App-intro">
+        Bye there!
+      </p>
+    </div>
+  );
+}
+
+export default App;
