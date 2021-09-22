5b9ef2da1b3cdcff1a87a767ac0e51d6bf0e4b74
Sean Cook <smcook55@gmail.com>
2021-09-22 12:37:35 -0500

    Feature request

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Test!
+          I was changed in feature branch!
         </p>
       </header>
       <p className="App-intro">
